45c9e19c5f66d6377d6101d4a2d9412714b435b1
matthewly <mmly2@illinois.edu>
2016-03-07 04:02:29 -0600

    lab 3 tutorial

--- source_js/app.js
+++ source_js/app.js
@@ -1,5 +1,16 @@
 var app = angular.module('mp3',['ngRoute']);
 
 app.config(function ($routeProvider) {
-
+	$routeProvider
+		.when('/gallery', {
+			templateUrl : 'partials/gallery.html',
+			controller: 'demoController'
+		})
+		.when('/list/:rank', {
+			templateUrl : 'partials/list.html',
+			controller : 'demoTwoController'
+		})
+		.otherwise({ // if url doesn't exist, go to /gallery
+			redirectTo: '/gallery'
+		});
 })
--- source_js/controllers.js
+++ source_js/controllers.js
@@ -1,1 +1,25 @@
 /* Sample Controller */
+app.controller('demoController', ['$scope', '$http', function($scope,$http) {
+	$scope.myName = "Matthew Ly";
+
+	$scope.classdata = {
+		"title": "CS498RK",
+		"names": ["matthew", "annie", "biplab", "devin"]
+	}
+
+	$scope.change = function() {
+		$scope.classdata.names.push($scope.inputValue);
+		$scope.inputValue = "";
+	}
+
+	$http.get('data/imdb250.json').success(function(data) {
+		$scope.oldStaff = data;
+		console.log(data);
+	}).error(function (err) {
+		console.log(err);
+	});
+}]);
+
+app.controller('demoTwoController', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams) {
+	$scope.myRank = $routeParams.rank;
+}]);
